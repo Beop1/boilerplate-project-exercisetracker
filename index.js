082110b09b727aaaa257d05d46796db74f4361d3
Bernardo <bernardo.vpp@gmail.com>
2023-11-21 21:29:55 -0300

    Update dependencies and add a body parser middleware. (generated by blackbox.ai )

--- index.js
+++ index.js
@@ -4,35 +4,40 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const crypto = require('crypto');
+let bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://bernardovpp:fQySC85zha5DVbPa@cluster0.e2avfpq.mongodb.net/?retryWrites=true&w=majority', 
-//                 {useNewUrlParser: true, useUnifiedTopology:  true});
-// var usernameSchema = new mongoose.Schema({
-//   username: String,
-//   _id: String
-// });
+mongoose.connect('mongodb+srv://bernardovpp:fQySC85zha5DVbPa@cluster0.e2avfpq.mongodb.net/?retryWrites=true&w=majority');
 
-// var User = mongoose.model('username', usernameSchema);
+var usernameSchema = new mongoose.Schema({
+  username: String,
+  _id: String
+});
+
+var User = mongoose.model('username', usernameSchema);
 
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 app.post('/api/users', (req, res) => {
-  let username = {
-    username: req.body.username,
-    _id: crypto.createHash('sha256').update(username).digest('hex')
-  };
-  // const hash = crypto.createHash('sha256');
-  // hash.update(username);
-  
-  // const _id = hash.digest('hex');
 
-  res.json({username});
+  let username = req.body.username;
+
+  const hash = crypto.createHash('sha256');
+  hash.update(username);
+
+  var user = new User({ username: req.body.username, _id: hash.digest('hex') });
+
+  user.save();
+
+  res.json({ username: user.username, _id: user._id });
 });
 
 
