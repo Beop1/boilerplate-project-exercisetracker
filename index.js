8bdd3874b1f14d13245d2d9af99cbff37879dc86
Bernardo <bernardo.vpp@gmail.com>
2023-11-23 02:16:01 -0300

    The commit changes the database schema for a user to include a date, duration, and description for exercises.
    It also adds a new route to get all exercises for a user. (generated by blackbox.ai )

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://bernardovpp:fQySC85zha5DVbPa@cluster0.e2avfpq.mongodb.net/?retryWrites=true&w=majority');
 
-var usernameSchema = new mongoose.Schema({
-  username: String
+var userSchema = new mongoose.Schema({
+  username: String,
+  date: String,
+  duration: Number,
+  description: String
 });
 
-var User = mongoose.model('username', usernameSchema);
+var User = mongoose.model('user', userSchema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -27,7 +30,6 @@ app.post('/api/users', (req, res) => {
   var user = new User({ username: req.body.username });
 
   user.save();
-
   res.json({ username: user.username, _id: user._id });
 });
 
@@ -40,15 +42,27 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
-app.post('/api/users/:_id?/exercises', async (req, res) => {
+app.post('/api/users/:_id/exercises', async (req, res) => {
   try {
-    var user = await User.findById({_id: req.params._id});
-    res.json({user});
-  } catch (err){
-    res.json(err);
+    let date = req.body.date !== "" ? new Date(req.body.date) : new Date();
+    // let user = await User.findOne({_id: req.params._id});
+
+    user = User.updateOne({_id: req.params._id}, {description: req.body.description}, {duration: req.body.duration}, {date: date }, {new:true});
+    user.save();
+
+    console.log(user);
+    res.json(user);
+    // res.json({ _id: req.params._id, username: user.username, date: date.toDateString(), duration: exercise.duration, description: exercise.description });
   }
+  catch (err) {
+    res.json(err);
+  };
 });
 
+app.get('/api/users/:_id/logs', (req, res) => {
+
+
+});
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
