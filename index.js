0f25add9a8c2a2ebec95ccfe4e2d199d0125b5e8
Bernardo <bernardo.vpp@gmail.com>
2023-12-07 14:53:46 -0300

    a

--- index.js
+++ index.js
@@ -3,66 +3,111 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-let bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://bernardovpp:fQySC85zha5DVbPa@cluster0.e2avfpq.mongodb.net/?retryWrites=true&w=majority');
 
-var userSchema = new mongoose.Schema({
-  username: String,
-  date: String,
-  duration: Number,
-  description: String
-});
-
-var User = mongoose.model('user', userSchema);
+var User = mongoose.model('user', { username: String });
+var Exercise = mongoose.model('exercise', { user_id: { type: String, required: true }, description: String, duration: Number, date: Date });
 
 app.use(cors())
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post('/api/users', (req, res) => {
-  var user = new User({ username: req.body.username });
+app.post('/api/users', async (req, res) => {
+  const userObj = new User({ username: req.body.username });
 
-  user.save();
-  res.json({ username: user.username, _id: user._id });
+  try {
+    const user = await userObj.save();
+    res.json(user);
+  }
+  catch (err) {
+    console.log(err);
+  }
 });
 
 app.get('/api/users', async (req, res) => {
   try {
-    let users = await User.find();
-    res.json(users);
-  } catch (err) {
+    let users = await User.find().select("_id username");
+    if (!users) res.send("No users found");
+    else res.json(users);
+  }
+  catch (err) {
     res.status(500).json(err);
   }
 });
 
 app.post('/api/users/:_id/exercises', async (req, res) => {
+
+  const id = req.params._id;
+  const { description, duration, date } = req.body;
+
   try {
-    let date = req.body.date !== "" ? new Date(req.body.date) : new Date();
-    // let user = await User.findOne({_id: req.params._id});
+    const user = await User.findById(id);
 
-    user = User.updateOne({_id: req.params._id}, {description: req.body.description}, {duration: req.body.duration}, {date: date }, {new:true});
-    user.save();
+    if (!user) {
+      res.send("Could not find user");
+    }
+    else {
+      const exerciseObj = new Exercise({
+        user_id: user._id,
+        description,
+        duration,
+        date: date ? new Date(date) : new Date()
+      });
 
-    console.log(user);
-    res.json(user);
-    // res.json({ _id: req.params._id, username: user.username, date: date.toDateString(), duration: exercise.duration, description: exercise.description });
+      const exercise = await exerciseObj.save();
+
+      res.json({
+        _id: user._id,
+        username: user.username,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: new Date(exercise.date).toDateString()
+      });
+    }
   }
-  catch (err) {
-    res.json(err);
-  };
+  catch (err) { console.log(err); }
 });
 
-app.get('/api/users/:_id/logs', (req, res) => {
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const { from, to, limit } = req.query;
+  const id = req.params._id;
+  const user = await User.findById(id);
 
+  if (!user) return res.send("Could not find user");
 
+  let dateObj = {};
+
+  if (from) dateObj["$gte"] = new Date(from);
+  if (to) dateObj["$lte"] = new Date(to);
+
+  let filter = {
+    user_id: id
+  }
+
+  if (from || to) filter.date = dateObj;
+
+  const exercises = Exercise.find(filter).limit(Number.parseInt(limit) ?? 500);
+
+  const log = (await exercises).map(e => ({
+    description: e.description,
+    duration: e.duration,
+    date: e.date.toDateString()
+  }));
+
+  res.json({
+    username: user.username,
+    count: exercises.length,
+    _id: user._id,
+    log
+  });
 });
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
